3aed878c792bcd66d38b9f91f5939f256b77fd16
kegor <egor.krushinov@gmail.com>
2017-10-23 23:45:58 +0300

    Changes for Quiz: SVM Author ID Accuracy

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -26,5 +26,12 @@ features_train, features_test, labels_train, labels_test = preprocess()
 ### your code goes here ###
 
 #########################################################
+from sklearn.metrics import accuracy_score
+from sklearn.svm import SVC
 
+clf = SVC(kernel="linear")
+clf.fit(features_train, labels_train)
+pred = clf.predict(features_test)
+
+print accuracy_score(pred, labels_test)
 
